ee0c64d1f2da278de12842d4458c00e5f564b120
ska24679 <ska24679@gmail.com>
2014-06-22 07:38:14 -0500

    added unzipping

--- get_data.R
+++ get_data.R
@@ -4,15 +4,23 @@ m=c("curl","wget","internal")
 tryCatch({
 		download.file(url,fn,method=m[1])
 		print(sprintf("Downloaded file using %s method",m[1]))
+		unzip("dataset.zip")
+		print("unzipped file")
 	},error = function(e) {
 		tryCatch({
 				download.file(url,fn,method=m[2])
 				print(sprintf("Downloaded file using %s method",m[2]))
+				unzip("dataset.zip")
+				print("unzipped file")
 			},error = function(e) {
 				download.file(url,fn,method=m[3])
 				print(sprintf("Downloaded file using %s method",m[3]))
+				unzip("dataset.zip")
+				print("unzipped file")
 			},error = function(e) {
 				print("I give up. Please download the file from the following url")
 				print(url)
-				print(sprintf("Then please rename it %s ",fn))
+				print("Then unzip it in the current directory")
+				print("Please ensure the resulting dataset is in a directory named:")
+				print("UCI HAR Dataset")
 	})})
